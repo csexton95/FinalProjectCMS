2c048c8081583cfbaf9402e442dde87cc8d1777a
csexton95 <csexton@localhost>
2015-05-06 15:45:19 -0400

    Human new

--- FinalProjectCMS/src/edu/saintjoe/cs/csexton95/FinalProjectCMS/Human.java
+++ FinalProjectCMS/src/edu/saintjoe/cs/csexton95/FinalProjectCMS/Human.java
@@ -1,5 +1,17 @@
 package edu.saintjoe.cs.csexton95.FinalProjectCMS;
 
-public class Human {
+public class Human extends Mammal {
+	private Human newHuman;
+	public Human(String name, int babies) {
+		super(name, babies);
+		}
 
+	public Human setBabies (Human newBabies) {
+		super.setBabies(newBabies.toString());
+		Human oldBabies = newHuman;
+		newHuman = newBabies;
+		return oldBabies;
+		}
+	
+	
 }
