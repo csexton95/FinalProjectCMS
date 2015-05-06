488d8bf12928df80f019826fa80461f0ea54d8b6
csexton95 <csexton@localhost>
2015-05-06 15:51:16 -0400

    Driver new

--- FinalProjectCMS/src/edu/saintjoe/cs/csexton95/FinalProjectCMS/Driver.java
+++ FinalProjectCMS/src/edu/saintjoe/cs/csexton95/FinalProjectCMS/Driver.java
@@ -4,7 +4,16 @@ public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Mammal mOne, mTwo, mTHree;
+		
+		mOne = newMammal("born", "Whale");
+		mTwo = newMammal("born", "Cat");
+		mThree = newMammal("born", "Human");
+		
+		System.out.println(mOne.toString());
+		System.out.println(mTwo.toString());
+		System.out.println(mThree.toString());
+		
 	}
 
 }
